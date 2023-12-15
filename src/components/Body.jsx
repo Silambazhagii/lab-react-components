959c39a6f49766ddedc6836651998d1a9fed9ed8
Silambazhagii <silambazhagii@gmail.com>
2023-12-15 16:01:57 +0530

    updated

---
+++ src/components/Body.jsx
@@ -0,0 +1,16 @@
+export function Body(data){
+    let imageData = data.props;
+    return(
+        <div>
+        {
+          imageData.map(image=>{
+            return(
+              <div key={image.id} className="column">
+                <img src={image.img} alt=""></img>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+}
